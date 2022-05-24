8399ec4b887ab755ca426045c3bdbe1902195dfa
Jaqueline Lin <XJACKIECO@gmail.com>
2022-05-24 13:03:45 -0400

    Update countDown.js

---
+++ countDown.js
@@ -0,0 +1,11 @@
+function countDown(time){
+    let timer = setInterval(function(){
+        time--;
+        if(time <=0){
+            clearInterval(timer);
+            console.log('Done');
+        } else {
+            console.log(time);
+        }
+    },1000)
+}
