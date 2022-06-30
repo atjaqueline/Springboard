e21876dc2d25bb1fab2792762467ec9c2943b769
Jaqueline Lin <XJACKIECO@gmail.com>
2022-06-30 16:05:16 -0400

    add files

---
+++ sql-querying/queries_products.sql
@@ -0,0 +1,8 @@
+-- Comments in SQL Start with dash-dash --
+
+ id | name  | price | can_be_returned 
+----+-------+-------+-----------------
+  3 | table |   124 | t
+  4 | door  |   500 | t
+(2 rows)
+
