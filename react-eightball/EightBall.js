97857d1eae549dd858b9ed120a8a2ec45f3b886a
Jaqueline Lin <XJACKIECO@gmail.com>
2022-10-01 18:55:07 -0400

    add files

---
+++ react-eightball/EightBall.js
@@ -0,0 +1,54 @@
+import React, {useState} from "react";
+import "./EightBall.css";
+
+EightBall.defaultProps  = {
+ answers: [
+    { msg: "It is certain.", color: "green" },
+    { msg: "It is decidedly so.", color: "green" },
+    { msg: "Without a doubt.", color: "green" },
+    { msg: "Yes - definitely.", color: "green" },
+    { msg: "You may rely on it.", color: "green" },
+    { msg: "As I see it, yes.", color: "green" },
+    { msg: "Most likely.", color: "green" },
+    { msg: "Outlook good.", color: "green" },
+    { msg: "Yes.", color: "green" },
+    { msg: "Signs point to yes.", color: "goldenrod" },
+    { msg: "Reply hazy, try again.", color: "goldenrod" },
+    { msg: "Ask again later.", color: "goldenrod" },
+    { msg: "Better not tell you now.", color: "goldenrod" },
+    { msg: "Cannot predict now.", color: "goldenrod" },
+    { msg: "Concentrate and ask again.", color: "goldenrod" },
+    { msg: "Don't count on it.", color: "red" },
+    { msg: "My reply is no.", color: "red" },
+    { msg: "My sources say no.", color: "red" },
+    { msg: "Outlook not so good.", color: "red" },
+    { msg: "Very doubtful.", color: "red" },
+  ]
+};
+
+function ranChoice(res) {
+    const ranIdx = Math.floor(Math.random() * res.length);
+    return res[ranIdx];
+}
+
+function EightBall(props) {
+    const [color, setColor] = useState('black');
+    const [msg, setMessage] = useState("Think of a Question");
+
+    function handleClick() {
+        const { msg, color } = ranChoice(props.answers);
+        setMessage(msg);
+        setColor(color);
+    }
+    return (
+        <div
+          className="EightBall"
+          onClick={handleClick}
+          style={{ backgroundColor: color }}
+        >
+          <b>{msg}</b>
+        </div>
+      );
+}
+
+export default EightBall;
