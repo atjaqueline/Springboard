025dac488d803dfd5f3264c8d66b7ec212875090
Jaqueline Coelho <88630180+jaquelinelin@users.noreply.github.com>
2023-08-05 22:24:48 -0400

    na

--- sql/contest-leaderboard.sql
+++ sql/contest-leaderboard.sql
@@ -8,3 +8,20 @@ on m.hacker_id = h.hacker_id
 group by m.hacker_id, h.name
 having total_score > 0
 order by total_score desc, m.hacker_id;
+
+SELECT GROUP_CONCAT(NUMB SEPARATOR '&')
+FROM (
+    SELECT @num:=@num+1 as NUMB FROM
+    information_schema.tables t1,
+    information_schema.tables t2,
+    (SELECT @num:=1) tmp
+) tempNum
+WHERE NUMB<=1000 AND NOT EXISTS(
+        SELECT * FROM (
+            SELECT @nu:=@nu+1 as NUMA FROM
+                information_schema.tables t1,
+                information_schema.tables t2,
+                (SELECT @nu:=1) tmp1
+                LIMIT 1000
+            ) t
+        WHERE FLOOR(NUMB/NUMA)=(NUMB/NUMA) AND NUMA<NUMB AND NUMA>1
