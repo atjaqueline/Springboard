a175117360e5d8b70de32336367209e626aa4e3b
Jaqueline Coelho <88630180+jaquelinelin@users.noreply.github.com>
2023-08-06 23:29:24 -0400

    n/a

--- sql/contest-leaderboard.sql
+++ sql/contest-leaderboard.sql
@@ -25,3 +25,21 @@ WHERE NUMB<=1000 AND NOT EXISTS(
                 LIMIT 1000
             ) t
         WHERE FLOOR(NUMB/NUMA)=(NUMB/NUMA) AND NUMA<NUMB AND NUMA>1
+
+
+SELECT GROUP_CONCAT(NUMB SEPARATOR '&')
+FROM (
+    SELECT @num:=@num+1 as NUMB FROM
+    information_schema.tables t1,
+    information_schema.tables t2,
+    (SELECT @num:=1) tmp
+) tempNum
+WHERE NUMB<=1000 AND NOT EXISTS(
+        SELECT * FROM (
+            SELECT @nu:=@nu+1 as NUMA FROM
+                information_schema.tables t1,
+                information_schema.tables t2,
+                (SELECT @nu:=1) tmp1
+                LIMIT 1000
+            ) t
+        WHERE FLOOR(NUMB/NUMA)=(NUMB/NUMA) AND NUMA<NUMB AND NUMA>1
